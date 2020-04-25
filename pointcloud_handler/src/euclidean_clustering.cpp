0bc371562f3bcfcab293161796ccaa6581cfad1c
ozangungortuhh <ozan.gungor@tuhh.de>
2020-04-25 19:57:29 +0200

    Add planar segmentation filter

--- pointcloud_handler/src/euclidean_clustering.cpp
+++ pointcloud_handler/src/euclidean_clustering.cpp
@@ -4,6 +4,11 @@
 #include <pcl/point_types.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
+// Segmentation specific includes
+#include <pcl/filters/extract_indices.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <iostream>
 
 class Cluster
 {
@@ -18,9 +23,55 @@ class Cluster
 
         void cloudCallback(const sensor_msgs::PointCloud2::ConstPtr& cloud)
         {
-            // TODO
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_rest (new pcl::PointCloud<pcl::PointXYZ>);
+            pcl::fromROSMsg(*cloud, *cloud_filtered);
 
+            // Set segmentation object
+            pcl::SACSegmentation<pcl::PointXYZ> seg;
+            pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+            pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
+            pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZ> ());
 
+            seg.setOptimizeCoefficients (true);
+            seg.setModelType (pcl::SACMODEL_PLANE);
+            seg.setMethodType (pcl::SAC_RANSAC);
+            seg.setMaxIterations (100);
+            seg.setDistanceThreshold (0.02);
+
+            int i=0, nr_points = (int) cloud_filtered->points.size ();
+            while (cloud_filtered->points.size () > 0.3 * nr_points)
+            {
+                // Segment the largest planar component from the remaining cloud
+                seg.setInputCloud (cloud_filtered);
+                seg.segment (*inliers, *coefficients);
+                if (inliers->indices.size () == 0)
+                {
+                std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+                break;
+                }
+
+                // Extract the planar inliers from the input cloud
+                pcl::ExtractIndices<pcl::PointXYZ> extract;
+                extract.setInputCloud (cloud_filtered);
+                extract.setIndices (inliers);
+                extract.setNegative (false);
+
+                // Get the points associated with the planar surface
+                extract.filter (*cloud_plane);
+
+                // Remove the planar inliers, extract the rest
+                
+                extract.setNegative (true);
+                extract.filter (*cloud_rest);
+                *cloud_filtered = *cloud_rest;
+            }
+
+            // publish the clustered PointCloud
+            sensor_msgs::PointCloud2 ros_cloud;
+            pcl::toROSMsg(*cloud_rest, ros_cloud);
+            ros_cloud.header.frame_id = "base_scan";
+            cloud_pub.publish(ros_cloud);
         }
     
     private:
@@ -33,7 +84,7 @@ class Cluster
 int main(int argc, char **argv)
 {
   //Initiate ROS
-  ros::init(argc, argv, "laser_to_cloud");
+  ros::init(argc, argv, "clustering");
   //Create an object of class SubscribeAndPublish that will take care of everything
   Cluster cluster;
   ros::spin();
