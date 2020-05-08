b0309f7e388f6fdbc5bc6996506ed21efa98cb63
ozangungortuhh <ozan.gungor@tuhh.de>
2020-05-08 13:34:00 +0200

    Added markers publisher

--- laser_pipeline/src/cloud_cluster.cpp
+++ laser_pipeline/src/cloud_cluster.cpp
@@ -24,6 +24,8 @@ class CloudCluster
             cloud_sub = nh.subscribe("/raw_cloud", 1, &CloudCluster::cloudCallback, this);
             // PointCloud publisher
             cluster_pub = nh.advertise<pcl::PointCloud<pcl::PointXYZ>>("/clustered_cloud", 1);
+            // MarkerArray publisher
+            markers_pub = nh.advertise<visualization_msgs::MarkerArray>("/cluster_markers", 0 );
         }
 
         void cloudCallback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& msg)
@@ -34,7 +36,7 @@ class CloudCluster
             
             // Call euclidean clustering
             pcl::PointCloud<pcl::PointXYZ> clustered_cloud;
-            clustered_cloud = euclideanClustering(filtered_cloud);
+            clustered_cloud = euclideanClustering(filtered_cloud, true);
             
             // Publish clustered_cloud
             cluster_pub.publish(clustered_cloud);
@@ -44,15 +46,13 @@ class CloudCluster
         ros::NodeHandle nh;
         ros::Subscriber cloud_sub;
         ros::Publisher cluster_pub;
+        ros::Publisher markers_pub;
         const int MIN_CLUSTER_SIZE = 3;
         const int MAX_CLUSTER_SIZE= 250;
         const float CLUSTER_TOLERANCE = 0.08;
         
-        pcl::PointCloud<pcl::PointXYZ> euclideanClustering(pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud)
-        {   
-            // Clustered cloud to be created by the method
-            pcl::PointCloud<pcl::PointXYZ>::Ptr clustered_cloud (new pcl::PointCloud<pcl::PointXYZ>);
-            
+        pcl::PointCloud<pcl::PointXYZ> euclideanClustering(pcl::PointCloud<pcl::PointXYZ>::Ptr& input_cloud, bool publish_markers)
+        { 
             // Creating the KdTree object for the search method of the extraction
             pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
             tree->setInputCloud (input_cloud);
@@ -68,6 +68,22 @@ class CloudCluster
             ec.extract (cluster_indices);
             std::vector<pcl::PointIndices>::const_iterator it;
             std::vector<int>::const_iterator pit;
+
+            // Clustered cloud to be created by the method
+            pcl::PointCloud<pcl::PointXYZ>::Ptr clustered_cloud (new pcl::PointCloud<pcl::PointXYZ>);
+            clustered_cloud->header.frame_id = "/base_scan";
+            
+            // Create marker and markerArray
+            visualization_msgs::MarkerArray markerArray;
+            visualization_msgs::Marker marker;
+            
+            if (publish_markers)
+            {
+                marker.header.frame_id = "/base_scan";
+                marker.header.stamp = ros::Time();
+                marker.type = visualization_msgs::Marker::CUBE;
+                marker.action = visualization_msgs::Marker::ADD;
+            }
 
             int cluster_count = 0;
             for(it = cluster_indices.begin(); it != cluster_indices.end(); ++it) 
@@ -86,11 +102,42 @@ class CloudCluster
                 // Merge current clusters to a single PointCloud to publish
                 *clustered_cloud += *single_cluster;
 
+                if (publish_markers)
+                {
+                    // Find min and max of cluster points to fit a box around them
+                    pcl::PointXYZ minPt, maxPt;
+                    pcl::getMinMax3D(*single_cluster, minPt, maxPt);
+    
+                    // Modify the marker
+                    marker.id = cluster_count;
+                    marker.pose.position.x = (maxPt.x+minPt.x)/2;
+                    marker.pose.position.y = (maxPt.y+minPt.y)/2;
+                    marker.pose.position.z = 0;
+                    marker.pose.orientation.x = 0.0;
+                    marker.pose.orientation.y = 0.0;
+                    marker.pose.orientation.z = 0.0;
+                    marker.pose.orientation.w = 1.0;
+                    marker.scale.x = (maxPt.x - minPt.x) + 0.2;
+                    marker.scale.y = (maxPt.y - minPt.y) + 0.2;
+                    marker.scale.z = 0.0;
+                    marker.color.a = 1.0; // Don't forget to set the alpha!
+                    marker.color.r = 0.0;
+                    marker.color.g = 1.0;
+                    marker.color.b = 0.0;
+                    
+                    // Add marker to markerArray
+                    markerArray.markers.push_back(marker);       
+                }
+
                 // Increase the cluster counter
                 ++cluster_count;
             }
+
+            if (publish_markers)
+            {
+                markers_pub.publish(markerArray);
+            }
             
-            clustered_cloud->header.frame_id = "/base_scan";
             return *clustered_cloud;
             // cluster_pub.publish(clustered_cloud);
         
