82f68d2491760d5d0a4b295be981f82041c964bc
ozangungortuhh <ozan.gungor@tuhh.de>
2020-05-08 12:37:40 +0200

    Final cloud_filter node

--- laser_pipeline/src/cloud_filter.cpp
+++ laser_pipeline/src/cloud_filter.cpp
@@ -1,9 +1,6 @@
 #include <ros/ros.h>
-// PCL specific includes
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include "pcl_ros/point_cloud.h"
 #include <pcl/filters/passthrough.h>
 
